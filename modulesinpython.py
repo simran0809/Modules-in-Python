f8cbba3a8e17911cce2e03a377c7e00b9dca36f3
Simran Vinodkumar Ahirwar <70149684+simran0809@users.noreply.github.com>
2024-10-23 23:38:23 +0530

    Add files via upload

---
+++ modulesinpython.py
@@ -0,0 +1,37 @@
+#Modules in python 
+
+
+import math 
+pie = math.pi
+print(pie)
+
+sq = 2*math.sqrt(2)
+print(sq)
+
+import random 
+random_number = random.randint(1,15)
+print( "Random number :",random_number) #random is a builtin modules.
+
+
+from math import pi
+print(pi)  #pi is a object .
+
+from math import *
+print(factorial(6))
+print(pi)
+print(sqrt(2))
+
+#########################################################################
+#########################################################################
+#################################################################################
+from math import *
+pie = pi 
+print(pie)
+
+sq = 2*sqrt(2) #squareroot is a function need to be arguement that is number 
+print(sq)
+
+from random import *
+random_number = randint(1,10)
+print(random_number)
+
